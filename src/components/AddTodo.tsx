f4ac0db83d18dde030af9c0414cd7ece86dfcdb6
ClaytonDewey <clay@dryadmedia.com>
2023-09-28 14:25:14 -0500

    Run through prettier

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -4,18 +4,18 @@ import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  const [input, setInput] = useState<string>('');
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [input, setInput] = useState<string>('')
+  const inputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     if (inputRef.current) {
-      inputRef.current.focus();
+      inputRef.current.focus()
     }
   }, [])
 
   const handleSubmission = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('form has been submitted');
+    e.preventDefault()
+    console.log('form has been submitted')
   }
 
   return (
@@ -25,11 +25,11 @@ export const AddTodo = () => {
           ref={inputRef}
           value={input}
           onChange={e => setInput(e.target.value)}
-          type='text'
-          className='w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white'
-          placeholder='start typing...' />
-        <button
-          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl">
+          type="text"
+          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
+          placeholder="start typing..."
+        />
+        <button className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl">
           Submit
         </button>
       </div>
